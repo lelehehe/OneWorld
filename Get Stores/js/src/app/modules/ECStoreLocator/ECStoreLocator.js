a39c6ed476e334923c550c1d1527c89e4deeaf72
Mike Zhang <mzseattle@gmail.com>
2015-07-13 19:11:37 -0700

    location week

--- Get Stores/js/src/app/modules/ECStoreLocator/ECStoreLocator.js
+++ Get Stores/js/src/app/modules/ECStoreLocator/ECStoreLocator.js
@@ -2,7 +2,8 @@ define('ECStoreLocatorData', function()
 {
 	'use strict';
 	
-	var ECStoreLocatorData = {};
+	var ECStoreLocatorData = {
+	};
 	
 	ECStoreLocatorData.Model = Backbone.Model.extend({
 		parse: function (response)
@@ -13,12 +14,19 @@ define('ECStoreLocatorData', function()
 	ECStoreLocatorData.Collection = Backbone.Collection.extend({
 		
 		initialize: function(options) {
+			this.latitude1 = 47.48534879985527;
+			this.latitude2 = 47.63006660014471;
+			this.longitude1 = -122.23984815953342;
+			this.longitude2 = -122.01564484046658;
 
 	    	//this.id = options.id;
 	  	}
 
-	,	url: function() {			
-			return '../EC-Store-Locator/services/ecGetStores.ss?filters=' + '[[%22isinactive%22,%22is%22,%22F%22],%22AND%22,[%22custrecord_latitude%22,%22between%22,47.48534879985527,47.63006660014471],%22AND%22,[%22custrecord_longitude%22,%22between%22,-122.23984815953342,-122.01564484046658]]'
+	,	url: function() {
+			var path = '../EC-Store-Locator/services/ecGetStores.ss?filters=' + '[["isinactive","is","F"],"AND",["custrecord_latitude","between",' + this.latitude1 + ' , ' + this.latitude2 +
+				'],"AND",["custrecord_longitude","between",' + this.longitude1 + ', ' + this.longitude2 + ']]';
+			return path;
+			//return '../EC-Store-Locator/services/ecGetStores.ss?filters=' + '[[%22isinactive%22,%22is%22,%22F%22],%22AND%22,[%22custrecord_latitude%22,%22between%22,47.48534879985527,47.63006660014471],%22AND%22,[%22custrecord_longitude%22,%22between%22,-122.23984815953342,-122.01564484046658]]'
 		}
 	
 	,	model: ECStoreLocatorData.Model
